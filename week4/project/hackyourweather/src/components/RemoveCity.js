89148a0fa630cecea282262fa71a5a814e0557ed
Ekmel Kalayci <ekmkal@gmail.com>
2021-04-05 12:07:42 +0200

    modified hackyourweather project for w4

---
+++ week4/project/hackyourweather/src/components/RemoveCity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CgCloseO } from 'react-icons/cg';
+
+const RemoveCity = ({ removeCity, cityId }) => {
+    return (
+        <button className="city-box__remove-button" onClick={() => removeCity(cityId)} >
+            <CgCloseO />
+        </button>
+    );
+};
+
+export default RemoveCity;
